9229b52eb3e3512aeffe2d3974fd8a82429a5753
GyuJeGal <69112154+GyuJeGal@users.noreply.github.com>
2023-10-10 21:30:29 +0900

    Initial Settings

---
+++ src/test/java/babamba/blooming/BloomingApplicationTests.java
@@ -0,0 +1,13 @@
+package babamba.blooming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
