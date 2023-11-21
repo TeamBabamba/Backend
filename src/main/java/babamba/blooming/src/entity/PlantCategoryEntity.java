3df099b9d891d85eddf99f38832fb2f7514c9626
gyujegal <lkjdee0329@gmail.com>
2023-11-21 20:04:03 +0900

    [#3]Feat: PlantCategoryEntity 추가

---
+++ src/main/java/babamba/blooming/src/entity/PlantCategoryEntity.java
@@ -0,0 +1,24 @@
+package babamba.blooming.src.entity;
+
+import babamba.blooming.config.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "PlantCategory")
+@NoArgsConstructor
+public class PlantCategoryEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT UNSIGNED")
+    private Long id;
+
+    @Column(nullable = false)
+    private String categoryName;
+
+}
