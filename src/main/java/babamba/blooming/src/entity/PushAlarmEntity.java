c6c8e6cfc1195af7c9c733a357a84ed03222fde2
gyujegal <lkjdee0329@gmail.com>
2023-11-21 20:04:26 +0900

    [#3]Feat: PushAlarmEntity 추가

---
+++ src/main/java/babamba/blooming/src/entity/PushAlarmEntity.java
@@ -0,0 +1,27 @@
+package babamba.blooming.src.entity;
+
+import babamba.blooming.config.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "Manage")
+@NoArgsConstructor
+public class PushAlarmEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT UNSIGNED")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity userEntity;
+}
