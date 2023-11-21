4689a34ffc45c90223d5b4853979435741f5f100
gyujegal <lkjdee0329@gmail.com>
2023-11-21 21:15:42 +0900

    [#4]Feat: TagRepository 추가

---
+++ src/main/java/babamba/blooming/src/repository/TagRepository.java
@@ -0,0 +1,9 @@
+package babamba.blooming.src.repository;
+
+import babamba.blooming.src.entity.TagEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<TagEntity, Long> {
+}
