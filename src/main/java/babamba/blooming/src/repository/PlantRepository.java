b2d25e49f31e8712c1bef9cfdd38d65daee5f539
gyujegal <lkjdee0329@gmail.com>
2023-11-29 19:46:15 +0900

    [#5]Feat: PlantRepository 메서드 추가 findAllByUserAndStatus() -> 모든 카테고리의 식물 전부 조회 findAllByUserAndPlantCategoryEntityAndStatus() -> 특정 카테고리의 식물 조회

--- src/main/java/babamba/blooming/src/repository/PlantRepository.java
+++ src/main/java/babamba/blooming/src/repository/PlantRepository.java
@@ -1,9 +1,19 @@
 package babamba.blooming.src.repository;
 
+import babamba.blooming.config.Status;
+import babamba.blooming.src.entity.PlantCategoryEntity;
 import babamba.blooming.src.entity.PlantEntity;
+import babamba.blooming.src.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlantRepository extends JpaRepository<PlantEntity, Long> {
+
+    List<PlantEntity> findAllByUserAndStatus(UserEntity user, Status status);
+
+    List<PlantEntity> findAllByUserAndPlantCategoryEntityAndStatus(UserEntity user, PlantCategoryEntity plantCategory, Status status);
+
 }
