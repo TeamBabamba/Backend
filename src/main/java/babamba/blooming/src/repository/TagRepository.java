9f54c1d68d996a491e90b20d8a1c69eb74d8a51d
gyujegal <lkjdee0329@gmail.com>
2023-11-29 17:05:25 +0900

    [#3]Delete: TagEntity 삭제

--- src/main/java/babamba/blooming/src/repository/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package babamba.blooming.src.repository;
-
-import babamba.blooming.src.entity.TagEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TagRepository extends JpaRepository<TagEntity, Long> {
-}
