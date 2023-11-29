ea47e25cd6a8b80280ddfca2e6df902163ff189f
gyujegal <lkjdee0329@gmail.com>
2023-11-29 19:44:46 +0900

    [#5]Feat: ManageRepository 메서드 추가 findAllByPlantAndCreatedAtAfterAndStatus 메서드 추가 -> 일주일 간의 관리 데이터 조회

--- src/main/java/babamba/blooming/src/repository/ManageRepository.java
+++ src/main/java/babamba/blooming/src/repository/ManageRepository.java
@@ -1,9 +1,16 @@
 package babamba.blooming.src.repository;
 
+import babamba.blooming.config.Status;
 import babamba.blooming.src.entity.ManageEntity;
+import babamba.blooming.src.entity.PlantEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface ManageRepository extends JpaRepository<ManageEntity, Long> {
+
+    List<ManageEntity> findAllByPlantAndCreatedAtAfterAndStatus(PlantEntity plantEntity, LocalDateTime createdAt, Status status);
 }
