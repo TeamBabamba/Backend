28192ac4eeb0fe81c24872a899928c5f23535902
gyujegal <lkjdee0329@gmail.com>
2023-11-30 16:01:40 +0900

    [#6]Feat: PlantRepository 메서드 추가 findByIdAndStatus() 메서드 추가

--- src/main/java/babamba/blooming/src/repository/PlantRepository.java
+++ src/main/java/babamba/blooming/src/repository/PlantRepository.java
@@ -8,9 +8,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PlantRepository extends JpaRepository<PlantEntity, Long> {
+
+    Optional<PlantEntity> findByIdAndStatus(Long plantId, Status status);
 
     List<PlantEntity> findAllByUserAndStatus(UserEntity user, Status status);
 
