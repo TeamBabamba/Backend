5ca8376de7a940511adb15da4e1255bb889f0e9e
gyujegal <lkjdee0329@gmail.com>
2023-11-30 17:09:23 +0900

    [#8]Feat: DeletePlantDto 생성 @RequestBody로 plantId 파싱

---
+++ src/main/java/babamba/blooming/src/dto/request/DeletePlantDto.java
@@ -0,0 +1,12 @@
+package babamba.blooming.src.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+public class DeletePlantDto {
+    private Long plantId;
+}
