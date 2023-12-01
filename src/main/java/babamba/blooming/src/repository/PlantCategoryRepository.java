4e23646007c7251c8ba20ff6cde3b14d24d28b0a
gyujegal <lkjdee0329@gmail.com>
2023-12-01 20:41:24 +0900

    [#12]Feat: PlantCategoryRepository 메서드 추가 findAllByStatus() 메서드 추가

--- src/main/java/babamba/blooming/src/repository/PlantCategoryRepository.java
+++ src/main/java/babamba/blooming/src/repository/PlantCategoryRepository.java
@@ -1,10 +1,13 @@
 package babamba.blooming.src.repository;
 
+import babamba.blooming.config.Status;
 import babamba.blooming.src.entity.PlantCategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlantCategoryRepository extends JpaRepository<PlantCategoryEntity, Long> {
-
+    List<PlantCategoryEntity> findAllByStatus(Status status);
 }
