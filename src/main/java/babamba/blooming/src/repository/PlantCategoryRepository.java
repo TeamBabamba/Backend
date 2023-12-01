9f8f383ec0eb6ac9b68ebdd55a4f8c21005327b1
gyujegal <lkjdee0329@gmail.com>
2023-12-01 22:02:56 +0900

    [#13]Feat: PlantCategoryRepository 메서드 추가

--- src/main/java/babamba/blooming/src/repository/PlantCategoryRepository.java
+++ src/main/java/babamba/blooming/src/repository/PlantCategoryRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PlantCategoryRepository extends JpaRepository<PlantCategoryEntity, Long> {
+
+    Optional<PlantCategoryEntity> findByIdAndStatus(Long categoryId, Status status);
+
     List<PlantCategoryEntity> findAllByStatus(Status status);
 }
